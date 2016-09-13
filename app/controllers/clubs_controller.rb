e5f4cce1a7b70e7ae623a1f5bb47c99db545fb24
Rhys <rphillips1@live.com.au>
2016-09-13 21:24:06 +0700

    Finish club signup

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,26 @@
 class ClubsController < ApplicationController
+
   def new
+  	@club = Club.new
   end
+
+  def create
+  	@club = Club.new(club_params)
+  	if @club.save
+      flash[:success] = "#{@club.name} successfully created. Welcome to Class Master!"
+  		redirect_to @club
+  	else
+  		render 'new'
+  	end
+  end
+
+  def show
+  	@club = Club.find(params[:id])
+  end
+
+  private
+
+  	def club_params
+  		params.require(:club).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-    get '/signup', to: 'clubs#new'
 	root 'static_pages#home'
+    get '/signup', to: 'clubs#new'
+    post '/signup', to: 'clubs#create'
 	get  '/help',    to: 'static_pages#help'
     get  '/about',   to: 'static_pages#about'
     get  '/contact', to: 'static_pages#contact'
+    resources :clubs
 end
