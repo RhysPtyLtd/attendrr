1140e097c712a849b2792687b1463f62a36d4d74
Rhys <rphillips1@live.com.au>
2016-09-13 12:39:39 +0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = " ")
 		base_title = "Class Master"
-		if page_title.empty?
+		if page_title.empty? || page_title.nil?
 			base_title
 		else
 			page_title + " | " + base_title
