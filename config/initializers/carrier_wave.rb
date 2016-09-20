9ce82fb44ae424dc38ff3a0485b359c53c463924
Rhys <rphillips1@live.com.au>
2016-09-20 09:22:59 +0700

    Image uploader

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIPNECCZSCXJM33BQ'],
-      :aws_secret_access_key => ENV['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'],
-      :region                => ENV['ap-southeast-2']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['classmaster1']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
