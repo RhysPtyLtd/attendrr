1ee9cf8e2482e1dd806a8bbac1bc0a5c3d3d6a83
Rhys <rphillips1@live.com.au>
2019-07-30 05:06:29 +1000

    Small changes

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -15,7 +15,7 @@ class ClubsController < ApplicationController
   	@club = Club.new(club_params)
   	if @club.save
       @club.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Success! Please check your email to activate your account."
       redirect_to root_url
   	else
   		render 'new'
