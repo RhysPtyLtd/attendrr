496445700f9e4c2c451bd4b2f6c1e2bd20500a16
Rhys <rphillips1@live.com.au>
2019-08-06 06:48:43 +1000

    Add active column to ranks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129094433) do
+ActiveRecord::Schema.define(version: 20190805204348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,8 +80,9 @@ ActiveRecord::Schema.define(version: 20180129094433) do
     t.string   "name"
     t.integer  "position"
     t.integer  "activity_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "active",      default: true
     t.index ["activity_id"], name: "index_ranks_on_activity_id", using: :btree
   end
 
