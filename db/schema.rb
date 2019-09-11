920f88ba00a3cb3a87ff5210fcaeedc2950a44f3
Aakanksha <ash@narola.email>
2019-09-11 14:17:19 +0530

    Show the ranks of the student at the time they attended the activity in student attendance sheet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190805204348) do
+ActiveRecord::Schema.define(version: 20190909100804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,9 @@ ActiveRecord::Schema.define(version: 20190805204348) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "attended"
+    t.integer  "rank_id"
     t.index ["activity_id"], name: "index_attendances_on_activity_id", using: :btree
+    t.index ["rank_id"], name: "index_attendances_on_rank_id", using: :btree
     t.index ["student_id"], name: "index_attendances_on_student_id", using: :btree
     t.index ["timeslot_id"], name: "index_attendances_on_timeslot_id", using: :btree
   end
