4a09f064147a75d99cbb68a2cdae6e78c814e11e
Rhys <rphillips1@live.com.au>
2019-09-12 19:05:34 +1000

    Students can be deactivated

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,7 @@ class StudentsController < ApplicationController
 
 	def index
 		if @club = current_club
-			@students = @club.students.all
+			@students = @club.students.where(active: true)
 			# @items = Item.paginate :page => params[:page], :per_page => 5
 
 
@@ -139,7 +139,7 @@ class StudentsController < ApplicationController
 	private
 
 		def student_params
-			params.require(:student).permit(:email, :address_line_1, :address_line_2, :city, :state, :postcode,
+			params.require(:student).permit(:active, :email, :address_line_1, :address_line_2, :city, :state, :postcode,
 											:phone1, :phone2, :first_name, :last_name, :dob, :parent1, :parent2,
 											:size, :picture, :notes, :payment_plan_id, :student_rank, rank_ids: [],
 											student_ranks_attributes: [:student_id, :rank_id, :active])
