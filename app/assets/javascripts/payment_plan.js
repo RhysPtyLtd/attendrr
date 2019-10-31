e81934225b0438b4f0bd8149ea2ff42804012257
Aakanksha <ash@narola.email>
2019-10-31 12:14:50 +0530

    Add payment plan type option while payment plan create

---
+++ app/assets/javascripts/payment_plan.js
@@ -0,0 +1,31 @@
+$(document).on("turbolinks:load",function() {
+	change_plan();
+	$('#plan_type').bind('change', function() {
+		change_plan();
+	});
+	function change_plan(){
+		if ($('#plan_type').val() ==  "Class package")
+		{
+			$('#div_classes_amount').show();
+			$('#div_frequency').hide();
+		}
+		else if ($('#plan_type').val()  == "Regular recurring payments")
+		{
+			$('#div_frequency').show();
+			$('#div_classes_amount').hide();
+		}
+	}
+
+	$('#payment_plan_btn').unbind().click(function(e){
+		if ($('#plan_type').val() == "Regular recurring payments" && $('#payment_plan_frequency').val() == "")
+		{
+			alert("You must have to select frequency for further process");
+			e.preventDefault();
+		}
+		else if ($('#plan_type').val() == "Class package" && $('#payment_plan_classes_amount').val() == "")
+		{
+			alert("You must have to add classes_amount for further process");
+			e.preventDefault();
+		}
+	});
+});
