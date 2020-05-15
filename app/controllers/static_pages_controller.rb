cb61591d121819f3fb5bb08ac7f135d2373d79e1
Rhys <rphillips1@live.com.au>
2020-05-15 16:49:30 +1000

    Calculate home page student numbers with TypeOfStudent module

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,10 @@ class StaticPagesController < ApplicationController
   	if logged_in?
       @student = current_club.students.build
   	  @total_prospects = current_club.students.includes(:payment_plan).where(payment_plans: {name: 'Prospect'})
-      @active_prospects = @total_prospects.where(active: true)
+      @prospects = TypeOfStudent.active_prospects(current_club)
       @absent_alert = current_club.absent_alert
       @absent_alert_activation_date = Date.today - current_club.absent_alert
-      @students = current_club.students.where(active: true)
+      @students = TypeOfStudent.active_enrolled(current_club)
     end
   end
 
