8312d7d21c53aca56258ca130278ad335bbac02c
Rhys <rphillips1@live.com.au>
2020-06-26 21:17:57 +1000

    Drop Photos table

--- db/schema.rb
+++ db/schema.rb
@@ -131,12 +131,6 @@ ActiveRecord::Schema.define(version: 2020_06_24_105254) do
     t.index ["club_id"], name: "index_payment_plans_on_club_id"
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.text "image_data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "ranks", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "position"
