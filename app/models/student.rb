7d755f66287001e55061e07c572e0286cd7a68a2
Rhys <rphillips1@live.com.au>
2020-08-17 19:01:44 +1000

    Remove required fields from students

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,16 +8,7 @@ class Student < ApplicationRecord
   validates :club_id, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, 
-            length: { maximum: 255 }, 
-            format: { with: VALID_EMAIL_REGEX }
-  validates :address_line_1, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :postcode, presence: true
-  validates :phone1, presence: true
-  validates :dob, presence: true
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validate :picture_size
   before_save { email.downcase! }
   before_save { state.upcase! }
