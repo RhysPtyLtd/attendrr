87f866a192f4e8cfce459b89ac0045ee29c92783
Alpish Singhal <alpishs@yahoo.com>
2020-08-22 15:50:18 +0530

    AT-62 Fixes
    AT-62 Fixes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,14 +1,13 @@
 $(document).on("turbolinks:load",function() {
 
-
-
     // Setup - add a text input to each footer cell
     var title = $('#products tfoot th:first').text();
     $('#products tfoot th:first').html( '<input type="text" placeholder="Search '+title+'" />' );
 
     var dataTable = $('#products').DataTable( {
-        searching: true,
-        "sDom":"ltipr",
+        "pagingType": "full_numbers",
+        "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50,100, "All"] ],
+        responsive: true,
         sAjaxSource: $('#products').data('source'),
         initComplete: function () {
             this.api().columns([1,2,3]).every( function () {
@@ -49,40 +48,40 @@ $(document).on("turbolinks:load",function() {
     });
 
     var dataTable = $('#metrics').DataTable( {
-    bPaginate: false,
-    bJQueryUI: true,
-    bProcessing: true,
-    aoColumnDefs : [
-    {
-       'bSortable' : false,
-       'aTargets' : [ 1, 2, 3, 4, 5 ]
-    }],
-    bServerSide: true,
-        "language": {
-         "infoFiltered": ""
-    },
+        bPaginate: true,
+        bJQueryUI: true,
+        bProcessing: true,
+        aoColumnDefs : [
+        {
+           'bSortable' : false,
+           'aTargets' : [ 1, 2, 3, 4, 5 ]
+        }],
+        bServerSide: true,
+            "language": {
+             "infoFiltered": ""
+        },
 
-    sAjaxSource: $('#metrics').data('source'),
-        initComplete: function () {
-            this.api().columns([1,2,3,4,5]).every( function () {
-              var total = 0
-              var sum = this
-              .data()
-              .reduce(function(a, b) {
-                var x = parseFloat(a) || 0;
-                var y = parseFloat(b) || 0;
-                total++;
-                return x + y;
-              }, 0);
-              sum = (this[0] != 3 && this[0] != 4)? ( sum.toFixed(2) / total.toFixed(2) ).toFixed(2) : sum
-              sum = (this[0] == 5)? sum+"%": sum;
-              $(this.footer()).html("<b>"+sum+"</b>");
-            });
-       }
+        sAjaxSource: $('#metrics').data('source'),
+            initComplete: function () {
+                this.api().columns([1,2,3,4,5]).every( function () {
+                  var total = 0
+                  var sum = this
+                  .data()
+                  .reduce(function(a, b) {
+                    var x = parseFloat(a) || 0;
+                    var y = parseFloat(b) || 0;
+                    total++;
+                    return x + y;
+                  }, 0);
+                  sum = (this[0] != 3 && this[0] != 4)? ( sum.toFixed(2) / total.toFixed(2) ).toFixed(2) : sum
+                  sum = (this[0] == 5)? sum+"%": sum;
+                  $(this.footer()).html("<b>"+sum+"</b>");
+                });
+           }
+        });
+        document.addEventListener("turbolinks:before-cache", function() {
+        if ($('#metrics').length == 1) {
+         dataTable.destroy();
+        }
     });
-    document.addEventListener("turbolinks:before-cache", function() {
-    if ($('#metrics').length == 1) {
-     dataTable.destroy();
-    }
- });
 });
