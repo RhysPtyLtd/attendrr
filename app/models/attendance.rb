519b0bea378b18f3fd52f18bffdd74d96207b9e9
Rhys <rphillips1@live.com.au>
2020-08-27 19:09:38 +1000

    Fix duplicate records bug

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,14 +3,22 @@ class Attendance < ApplicationRecord
   belongs_to :activity
   belongs_to :timeslot
   belongs_to :rank
+  before_save :check_duplicate_records
 
   def find_rank
-  	rank_id = self.student.ranks.where(activity_id: self.activity.id).first.id
-  	self.student.ranks.where(activity_id: self.activity.id).order("created_at desc").each do |rank|
-  		if rank.created_at > self.attended_on
-  			rank_id = rank.id
-  		end
-  	end
-  	rank_id
+    rank_id = self.student.ranks.where(activity_id: self.activity.id).first.id
+    self.student.ranks.where(activity_id: self.activity.id).order("created_at desc").each do |rank|
+      if rank.created_at > self.attended_on
+        rank_id = rank.id
+      end
+    end
+    rank_id
   end
+
+  def check_duplicate_records
+    if Attendance.where("timeslot_id =? AND Date(created_at) = DATE(?) AND student_id =? AND Date(attended_on) = DATE(?)", self.timeslot_id, Date.today, self.student_id, self.attended_on).count > 0
+      raise ActiveRecord::Rollback
+    end
+  end
+
 end
