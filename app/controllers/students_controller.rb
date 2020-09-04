943e10068065e30fc51fc266ffda162293afd8e8
Alpish Singhal <alpishs@yahoo.com>
2020-09-04 19:18:18 +0530

    AT-96 CHANGES
    AT-96 CHANGES

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -49,9 +49,9 @@ class StudentsController < ApplicationController
 		@first_ranks_of_activities = @student.first_ranks_of_activities
 		redirect_to root_url if @student.nil?
 		if @student.student_since.nil?
-			@student_since = @student.created_at.strftime("%d/%m/%Y")
+			@student_since = @student.created_at
 		else
-			@student_since = @student.student_since.strftime("%d/%m/%Y")
+			@student_since = @student.student_since
 		end
 	end
 
