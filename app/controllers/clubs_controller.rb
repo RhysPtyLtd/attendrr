c418db1682056e869367477caecc78b22439271f
Rhys <rphillips1@live.com.au>
2020-09-29 18:30:08 +1000

    Remove superfluous signup validations

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -11,7 +11,7 @@ class ClubsController < ApplicationController
   def new
   	@club = Club.new(absent_alert: 14)
     @subscription = Subscription.find(params[:subscription]) if params[:subscription]
-    @subscription = nil if @subscription.id == 1
+    @subscription = nil if (@subscription.nil?) || (@subscription.id == 1) 
     session[:subscription] = @subscription
   end
 
--- app/models/club.rb
+++ app/models/club.rb
@@ -7,7 +7,7 @@ class Club < ApplicationRecord
 	attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
 	before_save { email.downcase! }
-    before_save { state.upcase! }
+    before_save :upcase_state
     #before_save { owner_first_name.capitalize_name }
     #before_save { owner_last_name.capitalize_name }
 	validates :name, presence: true, length: { maximum: 50 } 
@@ -18,12 +18,6 @@ class Club < ApplicationRecord
 					  uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-    validates :address_line_1, presence: true, length: { maximum: 200 }
-    validates :city, presence: true, length: { maximum: 200 }
-    validates :state, presence: true, length: { maximum: 200 }
-    validates :postcode, presence: true
-    validates :country, presence: true, length: { maximum: 200 }
-    validates :phone1, presence: true, length: { maximum: 25, minimum: 8 }
     validates :owner_first_name, presence: true, length: { maximum: 200 }
     validates :owner_last_name, presence: true, length: { maximum: 200 }
     validate :picture_size
@@ -113,4 +107,9 @@ class Club < ApplicationRecord
             self.save!
         end
 
+        def upcase_state
+            unless self.state.nil?
+                self.state.upcase!
+            end
+        end
 end
